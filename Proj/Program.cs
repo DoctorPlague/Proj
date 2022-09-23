c043bed44e030e867251c18af693d81d82cef8c0
Савелий Сарбаев <pointdapolcop2@gmail.com>
2022-09-23 11:35:13 +0300

    Добавьте файлы проекта.

---
+++ Proj/Program.cs
@@ -0,0 +1,16 @@
+﻿using Proj;
+
+var simple = new SimpleEncryption();
+
+string word = "12345";
+
+int number = 10;
+
+string res = simple.Encrypt(word,number);
+
+Console.WriteLine(res);
+
+Console.WriteLine();
+
+Console.WriteLine(simple.Decrypt(res, number));
+
---
+++ Proj/SimpleEncryption.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj
+{
+    public class SimpleEncryption
+    {
+        public string Encrypt(string text, int number)
+        {
+            if (!string.IsNullOrEmpty(text) && number > 0)
+            {
+
+                for (int i = 0; i < number; i++)
+                {
+                    var left = new StringBuilder();
+                    var right = new StringBuilder();
+                    for (int j = 0; j < text.Length; j++)
+                    {
+                        if (j % 2 != 0)
+                        {
+                            left.Append(text[j]);
+                        }
+                        else
+                        {
+                            right.Append(text[j]);
+                        }
+                    }
+                    text = left.ToString() + right.ToString();
+                }
+                
+            }
+            return text;
+
+
+        }
+
+
+        public string Decrypt(string text, int number)
+        {
+            if (!string.IsNullOrEmpty(text) && number > 0)
+            {
+                int res= Math.Abs(text.Length - number); ;
+                if ((text.Length % 2) != 0)
+                {
+                    res--;
+                }
+
+                if (res > text.Length-1)
+                {
+                    res %= text.Length;
+                }
+
+
+                return Encrypt(text, res);
+            }
+            return text;
+        }
+    }
+}
